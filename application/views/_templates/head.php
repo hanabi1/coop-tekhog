7b28ea351e52556b46f9de54c82eaac276dbb215
sebastian <sebastian.lockwood@gmail.com>
2014-05-12 14:05:29 +0200

    Added google analytics tracking script

--- application/views/_templates/head.php
+++ application/views/_templates/head.php
@@ -13,6 +13,17 @@
                 <?php endif;?>
         </title>
         
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-50893517-1', 'samhallssar.se');
+          ga('send', 'pageview');
+
+        </script>        
+
         <!-- jQuery -->
         <script src="<?php echo URL; ?>public/js/jquery-1.11.0.min.js"></script>
         
