8c44771f9a4d31c406e65d9114c1c8b8c1e87428
Hannele <paivi.hannele@gmail.com>
2014-05-21 23:40:36 +0200

    Information text
    Added information text to the info-box div.

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -16,5 +16,9 @@
 
 <div class="info-box" id="information">
   <h3>Information</h3>
-  <p></p>
+  <p>Samhällssår är en antologi med __st kortfilmer som har producerats inför det svenska riksdagsvalet 2014. 
+  	 Vi är en samling filmare och konstnärer som har reagerat på att de senaste årens politik sällan gynnar 
+  	 majoriteten av oss människor. Vi vill påbörja samtal om vad vi behöver i Sverige och därför har vi gjort 
+  	 varsin film till denna antologi. Så titta, sprid, samtala och förändra!
+  </p>
 </div>
