b23682269ebf194a10cf04d1761acf25f0ed8f2a
Erika Berthsjö <erika@berthsjo.com>
2014-05-26 12:19:51 +0200

    deleted filmskapare

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -3,7 +3,6 @@
 		<nav>
 		     <ul>
 		     	<li><a href="#movies">Filmer</a></li>
-		        <li><a href="#moviemakers">Filmskapare</a></li>
 		        <li><a href="#information">Information</a></li>
 		        <li><a href="#eventbox">Visningar</a></li>
 		        <li><a href="#contact">Kontakt</a></li>
