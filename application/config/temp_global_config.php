e6874d37c1781041c160c7040eb6245090bcf486
Alexander Nilsson <alexander.nilsson83@gmail.com>
2014-05-28 16:50:53 +0200

    css-fixes

--- application/config/temp_global_config.php
+++ application/config/temp_global_config.php
@@ -25,7 +25,7 @@ define('FB_SECRET_KEY','edac4ad9d9a04ba8efa4d4bce7bee03a');
  * TEST PLAYLIST ID: ETsGmQ-25bTg_X1BPDyIU_zHPn2dm0XF
  * LIVE PLAYLIST ID: SwXUlPkaY_FZ7R9AsTo1yJi7wRthcBtx
  */
-define('YOUTUBE_PLAYLIST_ID' , 'SwXUlPkaY_FZ7R9AsTo1yJi7wRthcBtx'); //<- test id!
+define('YOUTUBE_PLAYLIST_ID' , 'ETsGmQ-25bTg_X1BPDyIU_zHPn2dm0XF'); //<- test id!
 
 //If constant is false then countdown timer is visible and everything else hidden.
 define('IS_SITE_LAUNCHED',TRUE);
